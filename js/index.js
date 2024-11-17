1051106beb755c4b0e8a6851ba9617792f96d943
nidjasim <polamekkartnija@gmail.com>
2024-11-17 12:59:29 +0100

    first_draft

--- js/index.js
+++ js/index.js
@@ -15,3 +15,13 @@ const smallFont = document.getElementById('small')
 smallFont.addEventListener('change', ()=>{
   console.log('change')
 })
+
+var i;
+var fib = [0, 1]; // Initialize array!
+
+for (i = 2; i <= 10; i++) {
+  // Next fibonacci number = previous + one before previous
+  // Translated to JavaScript:
+  fib[i] = fib[i - 2] + fib[i - 1];
+  console.log(fib[i]);
+}
